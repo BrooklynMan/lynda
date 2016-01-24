015248a57a36cc048ee1db45bcf5588a9225d6f0
adam mcbride <adam.mcbride@gmail.com>
2016-01-24 18:52:43 -0500

    chapter 3 complete

--- ios-swift/Chapter 03/Ch 3 Challenge/Ch 3 Challenge/ViewController.swift
+++ ios-swift/Chapter 03/Ch 3 Challenge/Ch 3 Challenge/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Methods
+//  Ch 3 Challenge
 //
 //  Created by adam mcbride on 1/24/16.
 //  Copyright © 2016 adam mcbride designs. All rights reserved.
@@ -10,31 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
-    var score:Int = 0
+    @IBOutlet weak var clickCount: UILabel!
+    var timesClicked:Int = 0
     
-    func didScore(points:Int) {
-        score += points
-        label.text = "Score: \(score)"
-    }
-    
-    @IBAction func didShootAlien(sender: AnyObject) {
-        didScore(1)
-    }
-    
-    @IBAction func didShootShip(sender: AnyObject) {
-        didScore(5)
-    }
-    
-    @IBAction func didShootBase(sender: AnyObject) {
-        didScore(10)
+    @IBAction func counterButton(sender: AnyObject) {
+        timesClicked++
+        clickCount.text = "\(timesClicked)"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        didScore(0)
     }
 
     override func didReceiveMemoryWarning() {
